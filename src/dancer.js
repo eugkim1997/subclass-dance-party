533b8bf60094021b99da9c0938013870ef4f47f7
Eugene Kim <eugkim1997@gmail.com>
2020-07-09 09:47:07 -0700

    Added two new dancer classes

--- src/dancer.js
+++ src/dancer.js
@@ -12,7 +12,7 @@ var Dancer = function(top, left, timeBetweenSteps) {
   Dancer.prototype.step = function() {
     // the basic dancer doesn't do anything interesting at all on each step,
     // it just schedules the next step
-    setTimeout(function() {this.step();}, this.timeBetweenSteps);
+    setTimeout(this.step.bind(this), this.timeBetweenSteps);
   };
 
   Dancer.prototype.setPosition = function(top, left) {
