bb13e3c3c1466e0eb863924c72f9b51ce7d88d36
Eugene Kim <eugkim1997@gmail.com>
2020-07-09 15:28:33 -0700

    specified new dancer actions

--- src/colorfulDancer.js
+++ src/colorfulDancer.js
@@ -1,5 +1,6 @@
 var ColorfulDancer = function(top, left, timeBetweenSteps) {
   Dancer.call(this, top, left, timeBetweenSteps);
+  //this.$node = $('<span class="colorfulDancer"></span>');
 };
 
 ColorfulDancer.prototype = Object.create(Dancer.prototype);
@@ -8,5 +9,9 @@ ColorfulDancer.prototype.constructor = ColorfulDancer;
 
 ColorfulDancer.prototype.step = function() {
   Dancer.prototype.step.call(this);
-  this.$node.toggle();
+  //this.$node.toggle();
+  var r = Math.floor(Math.random() * 255);
+  var g = Math.floor(Math.random() * 255);
+  var b = Math.floor(Math.random() * 255);
+  this.$node.css("border-color", "rgb("+ r + "," + g + "," + b + ")");
 };
--- src/slideDancer.js
+++ src/slideDancer.js
@@ -8,5 +8,8 @@ SlideDancer.prototype.constructor = SlideDancer;
 
 SlideDancer.prototype.step = function() {
   Dancer.prototype.step.call(this);
-  this.$node.toggle();
+  this.$node.css({
+    left:(Math.random()*$(window).width()-20),
+    top:(Math.random()*$(window).height()-20),
+  });
 };
